4bb65c4d73566b2706ee61733ac8135a954828a5
phy4cs <C.C.Symonds@leeds.ac.uk>
2020-08-20 17:07:33 +0100

    Changed spelling of celsius unit, and added missing argument identifier flags to call to year_collator.py

--- DIMCool_data_tool/Controller.sh
+++ DIMCool_data_tool/Controller.sh
@@ -4,19 +4,21 @@
 DATA_DIR='/nobackup/earsj/out/africap/DIMlookup/rawouts'
 
 # directory from which jobs will be run:
-NC_DIR='/nobackup/earsj/out/nc_outs'
+NC_DIR='/nobackup/chmcsy/out/nc_outs'
 
 # directory job std out / std err:
-JOB_OUT='/nobackup/earsj/job_out_consolidate'
+JOB_OUT='/nobackup/chmcsy/job_out_consolidate'
 
 # directory for completed job scripts:
-JOB_DONE='/nobackup/earsj/consolidate_completed'
+JOB_DONE='/nobackup/chmcsy/consolidate_completed'
 
 # conda install location
-CONDA_LOC='/nobackup/earsj/miniconda3'
+CONDA_LOC='/nobackup/chmcsy/miniconda3'
 
 # python script location
-PYLOC='/nobackup/earsj'
+PYLOC='~/iFEED_Prototype/DIMCool_data_tool'
+
+RUN_DIR=$PWD
 
 # number of parallel processes for rcp-level consolidation
 N_PROC='8'
@@ -25,10 +27,10 @@ mkdir -p $JOB_DONE
 mkdir -p $JOB_OUT
 
 # going to loop over these in turn, and submit a job for each cobination:
-CROPS="maize soybean potato groundnut"
-REGIONS="malawi zambia tanzania safrica"
-MODELS="bcc-csm1-1 BNU-ESM CSIRO-Mk3-6-0 GFDL-ESM2G IPSL-CM5A-LR MIROC-ESM MPI-ESM-LR MRI-CGCM3 NorESM1-M bcc-csm1-1-m CanESM2 CNRM-CM5 GFDL-CM3 GFDL-ESM2M IPSL-CM5A-MR MIROC5 MIROC-ESM-CHEM MPI-ESM-MR"
-RCPS="rcp26 rcp85"
+#CROPS="maize soybean potato groundnut"
+#REGIONS="malawi zambia tanzania safrica"
+#MODELS="bcc-csm1-1 BNU-ESM CSIRO-Mk3-6-0 GFDL-ESM2G IPSL-CM5A-LR MIROC-ESM MPI-ESM-LR MRI-CGCM3 NorESM1-M bcc-csm1-1-m CanESM2 CNRM-CM5 GFDL-CM3 GFDL-ESM2M IPSL-CM5A-MR MIROC5 MIROC-ESM-CHEM MPI-ESM-MR"
+#RCPS="rcp26 rcp85"
 
 CROPS="groundnut"
 REGIONS="malawi"
@@ -72,7 +74,7 @@ conda activate DIMdata
 
 cd ${PYLOC}
 
-python3 year_collator.py ${RCP_DATA_DIR} ${NC_DIR} ${N_PROC}
+python3 year_collator.py -d ${RCP_DATA_DIR} -o ${NC_DIR} -p ${N_PROC}
 EOF
         chmod 755 ${JOB_SCRIPT}
 
