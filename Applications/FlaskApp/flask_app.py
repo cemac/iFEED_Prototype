8ff7b3626130fb3eae0341b6e53e5ac1fb7cf331
Chris Symonds <46372880+cemac-ccs@users.noreply.github.com>
2022-03-21 10:43:56 +0000

    Update flask_app.py
    Added absolute path to db back in

--- Applications/FlaskApp/flask_app.py
+++ Applications/FlaskApp/flask_app.py
@@ -13,7 +13,7 @@ from .access import is_logged_in, is_logged_in_as_admin
 from .access import InsertUser, DeleteUser, AssignRole
 
 # Connect to database
-DATABASE = './iFEED.db'
+DATABASE = '/var/www/development/iFEED.db'
 #DATABASE = os.path.join(os.getcwd(),'iFEED.db')
 assert os.path.exists(DATABASE), "Unable to locate database"
 app.secret_key = 'secret'
